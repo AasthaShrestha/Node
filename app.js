ea19fa01e18ffaae330038d7bfe484ff2e5eec55
AasthaShrestha <aasthashrestha008@gmail.com>
2024-10-19 12:24:36 +0545

    vercel

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 require("dotenv").config();
 const express = require("express"); //requiring express package
-const { blogs,sequelize, users } = require("./model/index");
-
+const { blogs, sequelize, users } = require("./model");
 
 // const { where } = require("sequelize");
 
-const bcrypt=require("bcrypt");
-const { homePage, singleBlog, deleteBlog, createForm, servicePage, createBlog } = require("./controller/blogController");
-const { registerUser, loginUser, renderRegister, renderLogin } = require("./controller/authController");
+const bcrypt = require("bcrypt");
+const {
+  homePage,
+  singleBlog,
+  deleteBlog,
+  createForm,
+  servicePage,
+  createBlog,
+} = require("./controller/blogController");
+const {
+  registerUser,
+  loginUser,
+  renderRegister,
+  renderLogin,
+} = require("./controller/authController");
 
 const app = express(); //storing it in app, app vanney variable throughout use garxau
 const blogRoute = require("./routes/blogRoute");
 const authRoute = require("./routes/authRoute");
 
 app.set("view engine", "ejs"); //ejs le behind the scene kk env chainxa gardinxa
-require("./model/index");
+require("./model");
 app.use(express.urlencoded({ extended: true }));
 app.use("", blogRoute); //route link gardiyo
 app.use("", authRoute);
 
 // '/' ma gayo vane k dekhauneh vanne kura, req garepaxi response aaunai parne hunxa
-app.get("/",homePage);
+app.get("/", homePage);
 
-app.get("/blog/:id",singleBlog);
+app.get("/blog/:id", singleBlog);
 app.get("/delete/:id", deleteBlog);
 
-app.get("/create",createForm);
+app.get("/create", createForm);
 
 app.get("/about", (req, res) => {
   res.render("about.ejs");
@@ -35,15 +46,15 @@ app.get("/contact", (req, res) => {
   res.send("<h1>Thanks,</h1>You are in the Contact section");
 });
 
-app.get("/services",servicePage );
+app.get("/services", servicePage);
 
 app.post("/create", createBlog);
 
-app.get("/register",renderRegister) 
-app.post("/register", registerUser); 
+app.get("/register", renderRegister);
+app.post("/register", registerUser);
 
-app.get("/login",renderLogin); 
-app.post("/login",loginUser); 
+app.get("/login", renderLogin);
+app.post("/login", loginUser);
 
 app.use(express.static("public/css/"));
 app.use(express.static("./storage/"));
--- controller/blogController.js
+++ controller/blogController.js
@@ -1,4 +1,3 @@
-
 const { blogs } = require("../model");
 
 exports.homePage = async (req, res) => {
@@ -15,32 +14,32 @@ exports.singleBlog = async (req, res) => {
   res.render("singleBlog.ejs", { blog: blog });
 };
 
-exports.deleteBlog =async (req,res)=>{
-  const id= req.params.id
+exports.deleteBlog = async (req, res) => {
+  const id = req.params.id;
   await blogs.destroy({
-    where:{
-      id:id
-    }
-  })
-  res.redirect("/")
-}
+    where: {
+      id: id,
+    },
+  });
+  res.redirect("/");
+};
 
 exports.createForm = (req, res) => {
   res.render("create");
 };
 
 exports.createBlog = async (req, res) => {
-    const filename = req.file.filename;
-    const { title, subtitle, description } = req.body;
+  const filename = req.file.filename;
+  const { title, subtitle, description } = req.body;
 
-    await blogs.create({
-      title: title,
-      subtitle: subtitle,
-      description: description,
-      image: filename,
-    });
-    res.send("Blog added Successfully");
-  };
+  await blogs.create({
+    title: title,
+    subtitle: subtitle,
+    description: description,
+    image: filename,
+  });
+  res.send("Blog added Successfully");
+};
 
 exports.servicePage = (req, res) => {
   res.render("services.ejs", {
